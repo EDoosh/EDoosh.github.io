f214cc1d2a199356c16c4acaa5d02f3351c6d1f1
EDoosh <edoosh.yt@gmail.com>
2019-12-28 11:08:17 +1300

    Added a comparison between GDoosh and UGDBot

---
+++ src/gdooshugd.js
@@ -0,0 +1,63 @@
+gdooshugd = `[
+	{
+        "title": "Link your account to an in-game account.",
+        "gdoosh": "Yes.",
+        "ugd": "Yes."
+    }, {
+		"title": "Send new rated, daily, and weekly levels into a channel.",
+		"gdoosh": "Yes.",
+		"ugd": "In servers with 200+ members."
+	}, {
+		"title": "Display information about profiles and levels.",
+		"gdoosh": "Yes.",
+		"ugd": "Yes."
+	}, {
+        "title": "Get a user's correct CP count.",
+        "gdoosh": "Yes.",
+        "ugd": "No."
+    }, {
+        "title": "Get a player's icon.",
+        "gdoosh": "Yes.",
+        "ugd": "With the profile command, shows all."
+	}, {
+        "title": "Display information on the current Daily and Weekly levels.",
+        "gdoosh": "Yes.",
+        "ugd": "Yes."
+    }, {
+        "title": "Check a level's position in the featured section.",
+        "gdoosh": "No.",
+        "ugd": "Yes."
+    }, {
+		"title": "Search for levels through filters.",
+		"gdoosh": "Yes.",
+		"ugd": "No."
+	}, {
+        "title": "Search for levels by a specified author.",
+        "gdoosh": "Yes.",
+        "ugd": "Yes."
+    }, {
+        "title": "Get an up-to-date list of GD Moderators.",
+        "gdoosh": "Soon.",
+        "ugd": "Yes."
+    }, {
+        "title": "Send level requests to a specified channel.",
+        "gdoosh": "Soon.",
+        "ugd": "Yes."
+    }, {
+        "title": "Display server leaderboards from player's statistics",
+        "gdoosh": "No.",
+        "ugd": "Yes."
+    }, {
+        "title": "Get Gauntlets and Mappacks, as well as info on the levels inside.",
+        "gdoosh": "Yes.",
+        "ugd": "No."
+    }, {
+        "title": "Show & download song information based on ID.",
+        "gdoosh": "Yes.",
+        "ugd": "No."
+    }, {
+        "title": "Self-hostable.",
+        "gdoosh": "Yes.",
+        "ugd": "No."
+    }
+]`;
